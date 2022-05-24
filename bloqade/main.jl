6b9517d81a341014a77e532fb6ea0875bc7d0bf4
Roger-luo <rogerluo.rl18@gmail.com>
2022-05-24 21:07:42 +0000

    update benchmark to include chains

--- bloqade/main.jl
+++ bloqade/main.jl
@@ -6,29 +6,33 @@ using BloqadeCUDA
 using BenchmarkTools
 CUDA.allowscalar(false)
 
-# Simulate nonequilibrium dynamics of a 12-site ring of Rydberg atoms.
-#  This script generates Figure 2 in the example RydbergBlockade.
+# Define the pulse by specifying a maximum omega and delta.
+# Note that these parameters are computed using pulser_compare.py
+Omega_max= 9.179086064116243
+delta_0 = -20.397969031369428
+delta_f =  9.179086064116243
+
+# Define the timescales of the pulse
+Trise = 0.2
+Tramp = 3.6
+Tfall = 0.2
+total_time = Trise + Tramp + Tfall
+
+# Define the detuning and Rabi fields as a function of time
+Δ = piecewise_linear(clocks=[0.0, Trise, Trise + Tramp, total_time], values=[delta_0,delta_0,delta_f,delta_f]);
+Ω = piecewise_linear(clocks=[0.0, Trise, Trise + Tramp, total_time], values=[0,Omega_max,Omega_max,0]);
+
+function chain_benchmark_problem(nsites::Int, distance::Float64)
+    atoms = generate_sites(ChainLattice(), nsites, scale=distance)
+    h = rydberg_h(atoms; C = 5420158.53 , Δ, Ω)
+    reg = zero_state(nsites);
+    return SchrodingerProblem(reg, total_time, h)
+end
 
 function ring_benchmark_problem(nsites::Int, distance::Float64)
     R = distance/(2*sin(2*pi/(nsites)/2))                                       # Radius of the circle, using a little trigonometry
     pos = [(R*sin(i*2*pi/(nsites)), R*cos(i*2*pi/(nsites)) ) for i in 1:nsites] # Positions of each atom
     atoms = BloqadeLattices.AtomList(pos)                                         # Define the atom positions as an AtomList.
-
-    # Define the pulse by specifying a maximum omega and delta.
-    # Note that these parameters are computed using pulser_compare.py
-    Omega_max= 9.179086064116243
-    delta_0 =-20.397969031369428
-    delta_f =  9.179086064116243
-
-    # Define the timescales of the pulse
-    Trise = 2.
-    Tramp = 6.
-    Tfall = 2.
-    total_time = Trise + Tramp + Tfall
-
-    # Define the detuning and Rabi fields as a function of time
-    Δ = piecewise_linear(clocks=[0.0, Trise, Trise + Tramp, total_time], values=[delta_0,delta_0,delta_f,delta_f]);
-    Ω = piecewise_linear(clocks=[0.0, Trise, Trise + Tramp, total_time], values=[0,Omega_max,Omega_max,0]);
 
     # Define the Hamiltonian and problem instance
     h = rydberg_h(atoms; C = 5420158.53 , Δ, Ω)
@@ -37,21 +41,42 @@ function ring_benchmark_problem(nsites::Int, distance::Float64)
 end
 
 @info "setting up problems"
-problems = map(4:25) do n
+nqubits = 4:25
+problems = Dict()
+problems["ring"] = map(nqubits) do n
     ring_benchmark_problem(n, 9.0)
+end
+
+problems["chain"] = map(nqubits) do n
+    chain_benchmark_problem(n, 5.7)
 end
 
 results = Dict{String, Any}()
 
 @info "benchmarking CPU"
-results["CPU"] = map(problems) do prob
+results["ring (CPU)"] = map(problems["ring"]) do prob
     @info "benchmarking..." prob
     t = @benchmark emulate!($prob) setup=(set_zero_state!($(prob.reg)))
     minimum(t).time
 end
 
 @info "benchmarking CUDA"
-results["CUDA"] = map(problems) do prob
+results["ring (CUDA)"] = map(problems["ring"]) do prob
+    dprob = adapt(CuArray, prob)
+    @info "benchmarking..." prob=dprob
+    t = @benchmark CUDA.@sync(emulate!($dprob)) setup=(set_zero_state!($(dprob.reg)))
+    minimum(t).time
+end
+
+@info "benchmarking CPU"
+results["chain (CPU)"] = map(problems["chain"]) do prob
+    @info "benchmarking..." prob
+    t = @benchmark emulate!($prob) setup=(set_zero_state!($(prob.reg)))
+    minimum(t).time
+end
+
+@info "benchmarking CUDA"
+results["chain (CUDA)"] = map(problems["chain"]) do prob
     dprob = adapt(CuArray, prob)
     @info "benchmarking..." prob=dprob
     t = @benchmark CUDA.@sync(emulate!($dprob)) setup=(set_zero_state!($(dprob.reg)))
--- pulser/benchmarks.py
+++ pulser/benchmarks.py
@@ -1,13 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 17 09:06:07 2022
-
-Copied and adapted from Pasqual pulser example
-https://pulser.readthedocs.io/en/stable/tutorials/simulating.html
-
-@author: jwurtz
-"""
-
 from pulser import Pulse, Sequence, Register
 from pulser_simulation import Simulation
 from pulser.waveforms import RampWaveform
@@ -22,7 +12,35 @@ import matplotlib.pyplot as plt
 # from matplotlib.pyplot import *
 # close('all')
 
-def generate_seq(L):
+def generate_chain_seq(L, distance=5.7):
+    coords = np.array([i * distance for i in range(L)])
+    coords = np.array([coords, np.array([distance for i in range(L)])]).T
+    reg = Register.from_coordinates(coords, prefix='atom')
+
+    Omega_max= 9.179086064116243
+    delta_0 = -20.397969031369428
+    delta_f =  9.179086064116243
+
+    t_rise = 200   # Rise time, in nanoseconds
+    t_fall = 200   # Fall time, in nanoseconds
+    t_sweep =3600   # Sweep time, in nanoseconds
+
+    rise = Pulse.ConstantDetuning(RampWaveform(t_rise, 0., Omega_max), delta_0, 0.)
+    sweep = Pulse.ConstantAmplitude(Omega_max, RampWaveform(t_sweep, delta_0, delta_f), 0.)
+    fall = Pulse.ConstantDetuning(RampWaveform(t_fall, Omega_max, 0.), delta_f, 0.)
+
+    # Define a sequence using the pulser interface
+    seq = Sequence(reg, MockDevice)
+    seq.declare_channel('ising', 'rydberg_global')
+    seq.add(rise, 'ising')
+    seq.add(sweep, 'ising')
+    seq.add(fall, 'ising')
+    return seq
+
+
+seq = generate_chain_seq(5)
+
+def generate_ring_seq(L):
     ground_state_spacing = 1
     R_interatomic = 9
     radius = R_interatomic/(2*np.sin(np.pi/L * (ground_state_spacing)))
@@ -35,7 +53,7 @@ def generate_seq(L):
     print('Unit disk radius: {:0.6f}um'.format(R_interatomic))
 
     reg = Register.from_coordinates(coords, prefix='atom')
-    reg.draw(blockade_radius=R_interatomic, draw_half_radius=True, draw_graph = True)
+    # reg.draw(blockade_radius=R_interatomic, draw_half_radius=True, draw_graph = True)
 
     rydberg_blockade_energy = MockDevice.interaction_coeff / R_interatomic**6
 
@@ -44,9 +62,9 @@ def generate_seq(L):
     delta_0 = -2*rydberg_blockade_energy
     delta_f = 0.9*rydberg_blockade_energy
 
-    t_rise = 2000   # Rise time, in nanoseconds
-    t_fall = 2000   # Fall time, in nanoseconds
-    t_sweep =6000   # Sweep time, in nanoseconds
+    t_rise = 200   # Rise time, in nanoseconds
+    t_fall = 200   # Fall time, in nanoseconds
+    t_sweep =3600   # Sweep time, in nanoseconds
 
     rise = Pulse.ConstantDetuning(RampWaveform(t_rise, 0., Omega_max), delta_0, 0.)
     sweep = Pulse.ConstantAmplitude(Omega_max, RampWaveform(t_sweep, delta_0, delta_f), 0.)
@@ -62,9 +80,21 @@ def generate_seq(L):
 
 
 # Simulate evolution
-nqubits_list = range(4,26) # 4:20
+nqubits_list = range(4,26) # 4:25
 @pytest.mark.parametrize('nqubits', nqubits_list)
 def test_sim(benchmark, nqubits):
     benchmark.group = "ring"
-    sim = Simulation(generate_seq(nqubits), sampling_rate=0.1)
+    sim = Simulation(generate_ring_seq(nqubits), sampling_rate=0.1)
     benchmark(sim.run, progress_bar=False)
+
+
+@pytest.mark.parametrize('nqubits', nqubits_list)
+def test_sim(benchmark, nqubits):
+    benchmark.group = "chain"
+    sim = Simulation(generate_chain_seq(nqubits), sampling_rate=0.1)
+    benchmark(sim.run, progress_bar=False)
+
+
+# sim = Simulation(seq, sampling_rate=0.1)
+# results = sim.run()
+# print(np.sort(np.abs(np.array(results.get_final_state()))**2,axis=0)[-10::])

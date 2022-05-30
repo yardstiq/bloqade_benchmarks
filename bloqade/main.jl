d9a6bee85ed44e5aa58bfd91812621664aed29e8
Roger-luo <rogerluo.rl18@gmail.com>
2022-05-30 17:07:49 +0000

    fix allowscalar error on cuda & flush data after one benchmark is done

--- bloqade/main.jl
+++ bloqade/main.jl
@@ -56,54 +56,87 @@ function ring_benchmark_problem(nsites::Int, distance::Float64)
     return SchrodingerProblem(reg, total_time, h)
 end
 
+function set_zero_state_cuda!(reg)
+    CUDA.allowscalar() do
+        set_zero_state!(reg)
+    end
+end
+
+function flush_data(results)
+    @info "flushing data"
+    if isfile("data.json")
+        d = open("data.json") do f
+            JSON.parse(f)
+        end
+        merge!(results, d)
+    end
+
+    write("data.json", JSON.json(results))
+end
+
 @info "benchmark start"
+
+if isfile("data.json")
+    @info "removing old data file"
+    rm("data.json")
+end
 
 nqubits = 4:22
 results = Dict{String, Any}()
 results["ring (CPU)"] = map(nqubits) do n
     prob = ring_benchmark_problem(n, 9.0)
     @info "benchmarking..." prob
-    t = @benchmark emulate!($prob) setup=(set_zero_state!($(prob.reg)))
+    t = @benchmark emulate!($prob) setup=(set_zero_state_cuda!($(prob.reg)))
     minimum(t).time
 end
+
+flush_data(results)
 
 results["ring (CUDA)"] = map(nqubits) do n
     prob = ring_benchmark_problem(n, 9.0)
     dprob = adapt(CuArray, prob)
     @info "benchmarking..." prob=dprob
-    t = @benchmark CUDA.@sync(emulate!($dprob)) setup=(set_zero_state!($(dprob.reg)))
+    t = @benchmark CUDA.@sync(emulate!($dprob)) setup=(set_zero_state_cuda!($(dprob.reg)))
     minimum(t).time
 end
+
+flush_data(results)
 
 results["chain (CPU)"] = map(nqubits) do n
     prob = chain_benchmark_problem(n, 5.7)
     @info "benchmarking..." prob
-    t = @benchmark emulate!($prob) setup=(set_zero_state!($(prob.reg)))
+    t = @benchmark emulate!($prob) setup=(set_zero_state_cuda!($(prob.reg)))
     minimum(t).time
 end
+
+flush_data(results)
 
 results["chain (CUDA)"] = map(nqubits) do n
     prob = chain_benchmark_problem(n, 5.7)
     dprob = adapt(CuArray, prob)
     @info "benchmarking..." prob=dprob
-    t = @benchmark CUDA.@sync(emulate!($dprob)) setup=(set_zero_state!($(dprob.reg)))
+    t = @benchmark CUDA.@sync(emulate!($dprob)) setup=(set_zero_state_cuda!($(dprob.reg)))
     minimum(t).time
 end
+
+flush_data(results)
 
 nqubits = 4:25
 results["chain (subspace,CPU)"] = map(nqubits) do n
     prob = chain_benchmark_subspace_problem(n, 5.7)
     @info "benchmarking..." prob
-    t = @benchmark emulate!($prob) setup=(set_zero_state!($(prob.reg)))
+    t = @benchmark emulate!($prob) setup=(set_zero_state_cuda!($(prob.reg)))
     minimum(t).time
 end
+
+flush_data(results)
 
 results["chain (subspace,CUDA)"] = map(nqubits) do n
     prob = chain_benchmark_subspace_problem(n, 5.7)
     dprob = adapt(CuArray, prob)
     @info "benchmarking..." prob=dprob
-    t = @benchmark CUDA.@sync(emulate!($dprob)) setup=(set_zero_state!($(dprob.reg)))
+    t = @benchmark CUDA.@sync(emulate!($dprob)) setup=(set_zero_state_cuda!($(dprob.reg)))
     minimum(t).time
 end
 
-write("data.json", JSON.json(results))
+flush_data(results)
